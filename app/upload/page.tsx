459827b6b2b001af7d66f138751d9d9babb5ab65
wjlee <wjlee.ling@gmail.com>
2024-12-12 13:00:08 +0900

    feat: added image-upload button

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { Card, CardContent } from "@/app/components/ui/card";
 import exifr from "exifr";
 import Image from "next/image";
+import { BookImage } from "lucide-react";
 
 export default function UploadPage() {
   const [loading, setLoading] = useState(false);
@@ -54,7 +55,62 @@ export default function UploadPage() {
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-2xl font-bold mb-6">이미지 업로드</h1>
-      <div className="flex flex-col md:flex-row gap-6">
+      <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 md:grid-cols-6">
+        <div className="md:col-span-4 col-span-full relative">
+          {imageFile ? (
+            <Image
+              src={URL.createObjectURL(imageFile)}
+              alt="Uploaded Image"
+              width={1024}
+              height={1024}
+              className="object-contain rounded-lg"
+            />
+          ) : (
+            <label className="flex flex-col items-center justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10 cursor-pointer">
+              <BookImage className="w-10 h-10 mb-4" />
+              <div className="flex">
+                <span className="inline-block rounded-md bg-white font-semibold text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500">
+                  이미지 선택
+                </span>
+                <input
+                  type="file"
+                  accept="image/*"
+                  onChange={handleFileChange}
+                  className="hidden"
+                  id="fileInput"
+                />
+                <p className="pl-1">또는 드래그하여 파일 업로드</p>
+              </div>
+            </label>
+          )}
+        </div>
+        <div className="md:col-span-2 flex flex-col">
+          <label className="block text-lg/6 font-bold text-gray-900 bg-slack-500">
+            title
+          </label>
+          <div className="mt-2 bg-white rounded-md outline outline-1 -outline-offset-1 focus-within:outline focus-within:outline-slate-400">
+            <input
+              id="post-title"
+              name="post-title"
+              type="text"
+              className="block min-w-0 w-full grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline sm:text-sm/6"
+            />
+          </div>
+          <label className="block text-lg/6 font-bold text-gray-900 bg-slack-500 mt-4">
+            description
+          </label>
+          <div className="mt-2 bg-white rounded-md outline outline-1 -outline-offset-1 focus-within:outline focus-within:outline-slate-400">
+            <input
+              id="post-body"
+              name="post-body"
+              type="text"
+              className="block w-full min-w-0 min-h-20 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline sm:text-sm/6"
+            />
+          </div>
+        </div>
+      </div>
+
+      {/* <div className="flex flex-col md:flex-row gap-6">
         {imageFile && (
           <div className="flex-1 mb-6 relative">
             <Image
@@ -132,7 +188,7 @@ export default function UploadPage() {
             rows={3}
           />
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
