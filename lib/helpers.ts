17c30e3b881d6899dfad25f1bcba09f9d690ba8f
wjlee <wjlee.ling@gmail.com>
2024-12-19 14:50:41 +0900

    update: displayLocalTime for better locality in case dateOnly==true

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -5,13 +5,9 @@ export function displayLocalTime({
   utcDateTime: string;
   dateOnly?: boolean;
 }) {
-  var date = new Date(utcDateTime).toLocaleString();
-  date = dateOnly
-    ? date
-        .split(".")
-        .slice(0, 3)
-        .map((part) => part.trim())
-        .join("/")
-    : date;
+  var date = new Date(utcDateTime);
+  if (dateOnly) {
+    return date.toLocaleDateString();
+  }
   return date.toLocaleString();
 }
