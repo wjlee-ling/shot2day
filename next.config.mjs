8c21a7e7ae22fee96e4c52d45c6ac297e8364cd1
wjlee <wjlee.ling@gmail.com>
2024-12-22 13:44:16 +0900

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
+import { hostname } from "os";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [process.env.NEXT_PUBLIC_SUPABASE_URL.split("//")[1]],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: process.env.NEXT_PUBLIC_SUPABASE_URL.split("//")[1],
+      },
+    ],
+    // domains: [process.env.NEXT_PUBLIC_SUPABASE_URL.split("//")[1]],
   },
 };
 
