87522a92a27a71589d1ea41f1c47a464d2abc612
wjlee <wjlee.ling@gmail.com>
2024-12-26 18:37:23 +0900

    fix

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,15 +1,15 @@
-import { createClient } from "@supabase/supabase-js";
+import { supabase } from "@/lib/supabase";
 import { NextResponse } from "next/server";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+// const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
-if (!supabaseUrl || !supabaseServiceKey) {
-  throw new Error("Supabase URL or Service Key is not defined");
-}
+// if (!supabaseUrl || !supabaseServiceKey) {
+//   throw new Error("Supabase URL or Service Key is not defined");
+// }
 
 const bucketName = "shot2day-image";
-const supabase = createClient(supabaseUrl, supabaseServiceKey);
+// const supabase = createClient(supabaseUrl, supabaseServiceKey);
 
 export async function POST(request: Request) {
   const { id } = await request.json();
