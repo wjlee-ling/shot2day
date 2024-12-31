5dccb3825404a6109f0639e05f1187d0dfdcc096
wjlee <wjlee.ling@gmail.com>
2024-12-31 16:05:26 +0900

    feat: resize the image fit to the viewport

--- app/shots/[id]/page.tsx
+++ app/shots/[id]/page.tsx
@@ -1,22 +1,25 @@
 'use client';
-import {Navbar} from '@/app/components/Navbar';
+import { ImageItem } from "@/lib/types";
 import { useSearchParams } from 'next/navigation'
+import Image from 'next/image'
+
 
 export default function Shot() {
   const searchParams = useSearchParams();
-  const image = searchParams.get('image') ? JSON.parse(searchParams.get('image')!) : null; // 추가된 코드
-
+  const image: ImageItem = searchParams.get('image') ? JSON.parse(searchParams.get('image')!) : null;
+  const metadata = image?.metadata;
   return (
-    <div>
-    <Navbar/>
-    <div>
-      {image && (
-        <div>
-          <h1>{image.id}</h1>
-          <img src={image.imageUrl} alt={image.text} />
-        </div>
-      )}
-    </div>
+    <div className="flex h-[calc(100vh-var(--navbar-height))] overflow-hidden">
+      <main className="flex-1 flex flex-col">
+        {image && (
+          <>
+            <h1>{image.id}</h1>
+            <div className="relative flex-1">
+              <Image src={image.imageUrl} alt={image.text} fill style={{ objectFit: 'contain' }} sizes="100vw"/>
+            </div>
+          </>
+        )}
+        </main>
     </div>
   )
 }
