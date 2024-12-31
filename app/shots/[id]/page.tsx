85549726b1c17c6143aeea2360c7050a949aefe7
wjlee <wjlee.ling@gmail.com>
2024-12-31 16:55:32 +0900

    feat: added sidebar for img text and metadata for shots/ post

--- app/shots/[id]/page.tsx
+++ app/shots/[id]/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { ImageItem } from "@/lib/types";
+import Sidebar from "@/app/components/Sidebar";
 import { useSearchParams } from 'next/navigation'
 import Image from 'next/image'
 
@@ -7,19 +8,18 @@ import Image from 'next/image'
 export default function Shot() {
   const searchParams = useSearchParams();
   const image: ImageItem = searchParams.get('image') ? JSON.parse(searchParams.get('image')!) : null;
-  const metadata = image?.metadata;
   return (
-    <div className="flex h-[calc(100vh-var(--navbar-height))] overflow-hidden">
-      <main className="flex-1 flex flex-col">
+    <div className="flex h-[calc(100vh-var(--navbar-height))] overflow-hidden m-4 pb-4">
         {image && (
-          <>
-            <h1>{image.id}</h1>
-            <div className="relative flex-1">
+        <main className="flex-1 flex flex-col-reverse md:flex-row gap-4">
+            <div className="border-2 border-black w-1/4">
+              <Sidebar imageItem={image} />
+            </div>
+            <div className="relative flex-1"> {/* `relative` required for the `fill` attr. in the child Image tag */}
               <Image src={image.imageUrl} alt={image.text} fill style={{ objectFit: 'contain' }} sizes="100vw"/>
             </div>
-          </>
-        )}
         </main>
+        )}
     </div>
   )
 }
