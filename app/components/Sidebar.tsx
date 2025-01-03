b352db87e60829f90489a321589b710df971be49
wjlee <wjlee.ling@gmail.com>
2025-01-03 15:55:29 +0900

    fix: invalid date for datetime

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,15 +1,17 @@
 import { ImageItem } from "@/lib/types";
 import { displayLocalTime } from "@/lib/helpers";
 
-const Sidebar = ({ imageItem }: {imageItem:ImageItem}) => {
+const Sidebar = ({ imageItem }: { imageItem: ImageItem }) => {
   const { text, createdAt, metadata } = imageItem;
   const metadataList = Object.entries(metadata).map(([key, value]) => {
     return value ? (
       <div key={key} className="flex flex-row flex-1 content-center text-lg">
         <div className="bg-black text-white font-bold p-1">{key}</div>
         <div className="p-1">
-          {key === "createDate"
-            ? displayLocalTime({ utcDateTime: value })
+          {value instanceof Object
+            ? key === "DateTimeOriginal"
+              ? displayLocalTime({ utcDateTime: value.rawValue })
+              : null
             : value}
         </div>
       </div>
@@ -18,9 +20,7 @@ const Sidebar = ({ imageItem }: {imageItem:ImageItem}) => {
 
   return (
     <div className="flex flex-1 flex-col text-center">
-      <div className="text-xl text-bold p-2">
-        {text}
-      </div>
+      <div className="text-xl text-bold p-2">{text}</div>
       {metadataList}
     </div>
   );
